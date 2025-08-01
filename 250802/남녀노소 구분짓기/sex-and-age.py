43b13c7a99cff59a879afb6c3aacffa757a2124a
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-08-02 08:45:34 +0900

    Title: 남녀노소 구분짓기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250802/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,14 @@
+g = input()
+o = int(input())
+
+if g == '0':
+    if o>=19:
+        print('MAN')
+    else:
+        print('BOY')
+else:
+    if o>=19:
+        print('WOMAN')
+    else:
+        print('GIRL')
+
