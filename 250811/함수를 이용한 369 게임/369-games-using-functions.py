47c23c53630411bb0dd1b51bb5ce2c531d0f9a2e
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-08-11 14:50:30 +0900

    Title: 함수를 이용한 369 게임, Time: 95ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250811/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+cnt=0
+
+for i  in range(a, b+1):
+    if i%3==0:
+        cnt+=1
+        continue
+    else:
+        l = str(i)
+        for j in range(len(l)):
+            if l[j]=='3' or l[j]=='6' or l[j]=='9':
+                cnt+=1
+                continue 
+print(cnt)
