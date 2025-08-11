fb42647e6033fdb9b640102ba3b070cfb12bd5c9
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-08-11 14:57:20 +0900

    Title: 함수를 이용한 369 게임, Time: 86ms, Memory: 17MB, Status: Passed - Codetree

--- 250811/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250811/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,14 +1,17 @@
 a, b = map(int, input().split())
-cnt=0
 
-for i  in range(a, b+1):
-    if i%3==0:
-        cnt+=1
-        continue
-    else:
-        l = str(i)
-        for j in range(len(l)):
-            if l[j]=='3' or l[j]=='6' or l[j]=='9':
-                cnt+=1
-                continue 
-print(cnt)
+def sol(a,b):
+    cnt=0
+    for i  in range(a, b+1):
+        if i%3==0:
+            cnt+=1
+            continue
+        else:
+            l = str(i)
+            for j in range(len(l)): 
+                if l[j]=='3' or l[j]=='6' or l[j]=='9':
+                    cnt+=1
+                    break 
+    return cnt
+
+print(sol(a,b))
