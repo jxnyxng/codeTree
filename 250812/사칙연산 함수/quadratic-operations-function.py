049a5f1d7ebe06dedfcfadd902498e9cda7fde24
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-08-12 12:00:51 +0900

    Title: 사칙연산 함수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250812/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,22 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+def sol(a, o ,c):
+    if o=='+':
+        return a+c
+    elif o=='-':
+        return a-c
+    elif o=='/':
+        return a/c
+    elif o=='*':
+        return a*c
+    else:
+        return 'False'
+
+t = sol(a,o,c)
+
+if t=='False':
+    print(t)
+else:
+    print(f'{a} {o} {c} = {t}')
