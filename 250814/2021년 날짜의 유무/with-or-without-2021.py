0c183cc6c1090a6757fd2ae38f25c9e3aa2dc326
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-08-14 10:41:20 +0900

    Title: 2021년 날짜의 유무, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250814/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,27 @@
+M, D = map(int, input().split())
+
+def check_M(M):
+    if M<=12:
+        return True
+    else:
+        return False
+
+def check_D(M, D):
+    sd = 1
+    ed =31
+    if M%2==0:
+        ed-=1
+    if M==2:
+        ed-=1
+    
+    if D<=ed:
+        return True
+
+if check_M(M):
+    if check_D(M,D):
+        print('Yes')
+    else:
+        print('No')
+else:
+    print('No')
+
