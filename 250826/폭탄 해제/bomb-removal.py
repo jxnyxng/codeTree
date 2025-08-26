642488cd00fff3905b8af08a243cda7282d40c75
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-08-26 23:08:26 +0900

    Title: 폭탄 해제, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250826/폭탄 해제/bomb-removal.py
@@ -0,0 +1,12 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+class A:
+    def __init__(self, code, color, second):
+        self.code = print(f'code : {code}')
+        self.color = print(f'color : {color}')
+        self.second = print(f'second : {second}')
+
+a = A(unlock_code, wire_color, seconds)
+
+        
