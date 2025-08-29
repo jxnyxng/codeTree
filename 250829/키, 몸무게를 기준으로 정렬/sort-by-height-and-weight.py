b9342a8bdff17b5b9cd119857fa05df94a0479c2
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-08-29 21:50:49 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250829/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,27 @@
+n = int(input())
+name = []
+height = []
+weight = []
+for _ in range(n):
+    n_i, h_i, w_i = input().split()
+    name.append(n_i)
+    height.append(int(h_i))
+    weight.append(int(w_i))
+
+
+class A:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+    
+answer = []
+
+for i in range(n):
+    answer.append(A(name[i], height[i], weight[i]))
+
+answer.sort(key=lambda x:(x.height, -x.weight))
+
+for i in answer:
+    print(i.name, i.height, i.weight)
+    
