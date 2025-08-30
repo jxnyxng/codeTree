247d2f4b5969efbc4a246755100917740fdf495a
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-08-30 23:54:32 +0900

    Title: 정렬된 수 위치 알아내기, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250830/정렬된 수 위치 알아내기/indices-of-sorted-array.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = list(map(int, input().split()))
+class Number:
+    def __init__(self, num, idx = -1):
+        self.num = num
+        self.idx = idx
+
+sequence = [Number(num) for num in arr]
+
+sorted_sequence = sorted(sequence, key = lambda x:x.num)
+
+for i, number in enumerate(sorted_sequence):
+    number.idx = i+1
+
+for number in sequence:
+    print(number.idx, end =' ')
