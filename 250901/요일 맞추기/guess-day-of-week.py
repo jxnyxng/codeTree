f2cb18b861ff4c2b503a8f2049b2df8ccdb3ffcd
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-09-01 09:46:06 +0900

    Title: 요일 맞추기, Time: 46ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 250901/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,21 @@
+m1, d1, m2, d2 = map(int, input().split())
+day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+Mo1=0
+Mo2=0
+
+for i in range(1, m1+1):
+    Mo1 += day[i]
+
+for i in range(1, m2+1):
+    Mo2 += day[i]
+
+
+gap1 = Mo1*(m1-1) + d1
+gap2 = Mo2*(m2-1) + d2
+    
+gap = gap2 - gap1
+check = gap%7
+
+answer = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+print(answer[check])
