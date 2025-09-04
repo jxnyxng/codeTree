117da47271909ff608578217cfe24c301deba47d
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-09-04 22:31:08 +0900

    Title: 진수 to 진수, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250904/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+n = input()
+# a 진수인 n
+# 을 b 진수로
+temp = 0
+for i in range(len(n)-1, -1, -1):
+    temp += (int(n[i]) * (a**i))
+ans = []
+
+while temp>=2:
+    ans.append(temp%b)
+    temp//=b
+
+ans.append(temp%b)
+for i in ans[::-1]:
+    print(i, end='')
+
