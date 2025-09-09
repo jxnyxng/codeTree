1d9eb8be3fa786a9e21524045e136e5b0b53ff8d
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-09-09 11:18:56 +0900

    Title: 방향에 맞춰 이동, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250909/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,25 @@
+n = int(input())
+moves = [tuple(input().split()) for _ in range(n)]
+dir = [move[0] for move in moves]
+dist = [int(move[1]) for move in moves]
+
+# Please write your code here.
+dx = [1, -1 , 0, 0]
+dy = [0, 0, -1, 1]
+
+x = 0
+y = 0
+
+for i in range(n):
+    if dir[i]=='E':
+        idx = 0
+    elif dir[i]=='W':
+        idx = 1
+    elif dir[i]=='S':
+        idx = 2
+    elif dir[i]=='N':
+        idx = 3
+
+    x += (dx[idx]*dist[i])
+    y += (dy[idx]*dist[i])
+print(x, y)
