5219c789e9342d4f2b0c3a60d35050c9af38b9e2
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-11-16 14:15:52 +0900

    Title: 금 채굴하기, Time: 68ms, Memory: 17MB, Status: Passed - Codetree

--- 251116/금 채굴하기/gold-mining.py
+++ 251116/금 채굴하기/gold-mining.py
@@ -10,42 +10,46 @@ dxs, dys = [1,-1,0,0], [0,0,1,-1]
 
 def in_range(i,j):
     return 0<=i<n and 0<=j<n
-
-q = deque()
-def bfs(i, j, k):
+    
+def bfs(i, j):
     global ans
+    
+    q = deque()
     visited = [[False] * n for _ in range(n)] 
-    q.append((i,j,k))
+    
+    q.append((i,j))
+    visited[i][j] = True
 
-    gold_cnt = 0 
-    gain = 0
-    cost = (k*k) + ((k+1) * (k+1))
+    gold_cnt = 0  
+    k = 0
 
     while q:
-        nowi, nowj, nowk = q.popleft() 
-        # 현재 위치가 금이면 카운트
-        if not visited[nowi][nowj] and grid[nowi][nowj] == 1:
-            gold_cnt+=1
-            gain+=m
-         
-        visited[nowi][nowj] = True
+        q_len = len(q)
+        
+        for _ in range(q_len):
+            nowi, nowj = q.popleft()
 
-        # 기회가 있을 때만
-        if nowk!=0: 
+            if grid[nowi][nowj] == 1:
+                gold_cnt += 1
+            
             for idx in range(4):
-                newi, newj = nowi + dys[idx], nowj+dxs[idx] 
+                newi, newj = nowi + dys[idx], nowj + dxs[idx]
                 if in_range(newi, newj) and not visited[newi][newj]:
-                    q.append((newi,newj,nowk-1))
-                 
-    if (gain-cost)>=0:
-        ans=max(ans, gold_cnt)
-    
+                    visited[newi][newj] = True
+                    q.append((newi,newj))
+        
+        cost = (k*k) + ((k+1) * (k+1))
+        gain = gold_cnt * m
+             
+        if (gain-cost) >= 0:
+            ans=max(ans, gold_cnt)
+        
+        k += 1
 
 ans = 0
-# 마름모 크기 결정 후 시작점 결정
-# k가 n-1 이면 다 덮는다
-for k in range(n):
-    for i in range(n): 
-        for j in range(n):
-            bfs(i,j,k) 
+
+for i in range(n): 
+    for j in range(n):
+        bfs(i,j)
+
 print(ans)
