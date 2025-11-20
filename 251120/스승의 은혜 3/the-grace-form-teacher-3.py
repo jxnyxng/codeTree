60cf448f175fff02659e01d16bcb3aeade39c383
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-11-20 10:37:30 +0900

    Title: 스승의 은혜 3, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251120/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 251120/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,22 +1,24 @@
-from itertools import permutations
-    
 N, B = map(int, input().split())
 gifts = [tuple(map(int, input().split())) for _ in range(N)]
-P = [gift[0] for gift in gifts]
-S = [gift[1] for gift in gifts]
-
-ans = 0
-
+ 
+ans=0
 # 쿠폰 적용할 선물 고르기
-for i in range(N):
-    nprice = P[:]
-    nprice[i] //= 2
-    price = 0
-    # 가능한 선물 조합
-    for j in range(1, N+1):
-        for newP in permutations(nprice, j):
-            if sum(newP)<=B:
-                ans = j
-                break
+for i in range(N): 
+    p = []
+    for j in range(N):
+        if i==j:
+            p.append((gifts[j][0]//2) + gifts[j][1])
+        else:
+            p.append(gifts[j][0] + gifts[j][1])
+    p.sort()
+    count=0
+    total=0
+    for cost in p:
+        if total + cost <= B:
+            total += cost
+            count += 1
+        else:
+            break
+    ans = max(ans, count)
 
 print(ans)
