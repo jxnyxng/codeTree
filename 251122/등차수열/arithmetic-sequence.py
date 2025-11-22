35aa1c7266b699f54fb3283b3d69a00dfad953e8
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-11-22 16:45:48 +0900

    Title: 등차수열, Time: 66ms, Memory: 17MB, Status: Passed - Codetree

--- 251122/등차수열/arithmetic-sequence.py
+++ 251122/등차수열/arithmetic-sequence.py
@@ -1,12 +1,15 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-# 리스트의 원소들 두개를 잡아서 뺀 값이 짝수이면 카운트
-cnt=0
-for i in range(n-1):
-    for j in range(i+1, n):
-        check=abs(a[i]-a[j])
-        if check!=0 and (check%2) == 0:
-            cnt+=1
+# 리스트의 원소들 두개를 잡아서 뺀 값이 짝수이면 카운트 
+max_num = max(a)
+ans = 0
+for K in range(1, max_num+1):
+    cnt=0
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if abs(K-a[i]) == abs(K-a[j]):
+                cnt+=1
+    ans = max(ans, cnt)
 
-print(cnt)
+print(ans)
