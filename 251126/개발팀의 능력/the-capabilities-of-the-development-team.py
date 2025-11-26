424fb6dd0c6599c3f7c921a3479f552be936b802
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-11-26 15:03:24 +0900

    Title: 개발팀의 능력, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251126/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 251126/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -7,13 +7,16 @@ for i in range(5):
     for j in range(5):
         if i==j:
             continue
-        for k in range(j+1, 5): 
+        for k in range(5): 
             if k==i:
                 continue
+            first = arr[i]
+            second = arr[j]+arr[k]
+            third = s-arr[i]-arr[j]-arr[k]
 
             # print(new_arr)
-            if not(arr[j]+arr[k] == arr[i] and arr[i]==s-arr[i]-arr[j]-arr[k]):
-                new_arr = [arr[j]+arr[k], arr[i], s-arr[i]-arr[j]-arr[k]]
+            if not(first==second or second==third or first==third):
+                new_arr = [first, second, third]
                 ans = min(ans, (max(new_arr)-min(new_arr)))
 
 if ans==float('inf'):
