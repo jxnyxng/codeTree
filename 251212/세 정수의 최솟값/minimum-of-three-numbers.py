57678d7d512fe4fc8a6fb5bd3309b761632a270b
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2025-12-12 10:41:48 +0900

    Title: 세 정수의 최솟값, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251212/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 251212/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -1,8 +1,9 @@
 a, b, c = map(int, input().split())
 
-if a<=b<=c or a<=c<=b:
-    print(a)
-elif b<=a<=c or b<=c<=a:
-    print(b)
-else:
-    print(c)
+m = a
+if m>b:
+    m=b
+if m>c:
+    m=c
+
+print(m)
