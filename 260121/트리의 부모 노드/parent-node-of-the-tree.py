06a04c3c0078ac23f124b409d08190dc8c7c9af5
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2026-01-21 16:18:50 +0900

    Title: 트리의 부모 노드, Time: 404ms, Memory: 40MB, Status: Passed - Codetree

--- 260121/트리의 부모 노드/parent-node-of-the-tree.py
+++ 260121/트리의 부모 노드/parent-node-of-the-tree.py
@@ -1,9 +1,25 @@
 n = int(input())
-edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
 
+tree = [[] for _ in range(n+1)]
 parent = [0] * (n+1)
-for n1, n2 in edges:
-    parent[n2] = n1
+visited = [False] * (n+1)
+st = 1
+visited[st] = True
 
-for p in range(2, n+1):
-    print(parent[p])
+for _ in range(n-1):
+    n1, n2 = map(int, input().split())
+    tree[n1].append(n2)
+    tree[n2].append(n1)
+
+
+def dfs(now):
+    for next_node in tree[now]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            parent[next_node] = now
+            dfs(next_node)
+
+dfs(st)
+
+for idx in range(2, n+1):
+    print(parent[idx])
