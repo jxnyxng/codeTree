9438fce2f1d404c8eea92f8a098a17637bf65b3c
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2026-01-22 14:52:06 +0900

    Title: 트리 노드 제거, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/트리 노드 제거/remove-tree-node.py
+++ 260122/트리 노드 제거/remove-tree-node.py
@@ -2,36 +2,33 @@ n = int(input())
 parent = list(map(int, input().split()))
 remove_node = int(input())
 
-# 0 based
-# tree 만들고
-# 끊을 노드의 visited를 미리 방문처리
-# 루트노드에서 시작해서 노드 개수 구하기
-
 tree = [[] for _ in range(n)]
 stnode = 0
 for idx in range(n):
     pnode, cnode = parent[idx], idx
-    if pnode == -1: stnode = idx
-    tree[pnode].append(cnode)
+    if pnode == -1: 
+        stnode = idx
+    else: 
+        if idx == remove_node:
+            continue
+        tree[pnode].append(cnode)
     
-
-visited = [False] * n
-# visited[stnode] = True
-visited[remove_node] = True
 ans=0
-
 def dfs(now):
     global ans
 
-    if visited[now]: return 
+    if not tree[now]:
+        ans+=1
+        return
 
-    visited[now] = True
-    
+    has_child = False
     for next_node in tree[now]:
-        if not visited[next_node]:
-            ans+=1
-            visited[next_node] = True
+        if next_node != remove_node:
+            has_child = True
             dfs(next_node)
-
-dfs(stnode)
+    
+    if not has_child:
+        ans += 1
+if stnode != remove_node:
+    dfs(stnode)
 print(ans)
