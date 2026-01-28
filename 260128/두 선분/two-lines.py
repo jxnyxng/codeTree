e4d1ae0e970d6210fb88b3993414a2cd7c9a9f92
KIM JUN YONG <154650820+jxnyxng@users.noreply.github.com>
2026-01-28 10:49:10 +0900

    Title: 두 선분, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260128/두 선분/two-lines.py
+++ 260128/두 선분/two-lines.py
@@ -1,6 +1,16 @@
 x1, x2, x3, x4 = map(int, input().split())
 
-if x3 <= x2:
-    print("intersecting")
-else:
-    print("nonintersecting")
+arr = [0] * 105
+
+for i in range(x1, x2+1):
+    arr[i] += 1
+
+for i in range(x3, x4+1):
+    arr[i] += 1
+
+for now in arr:
+    if now==2:
+        print("intersecting")
+        exit()
+
+print("nonintersecting")
