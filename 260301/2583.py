02c8add0f9700801bf22c587c180de9b4323cad6
kjy <kim0607mi@smail.kongju.ac.kr>
2026-03-01 23:46:29 +0900

    BOJ:2583

---
+++ 260301/2583.py
@@ -0,0 +1,44 @@
+# 백준 2583번
+
+from collections import deque
+
+m, n, k = map(int, input().split())
+graph = [[0] * n for _ in range(m)]
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2):
+        a = m - y1 - 1
+        b = m - y2 - 1
+        for j in range(a, b, -1):
+            graph[j][i] = 1
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    graph[x][y] = 1
+    size = 1
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
+                graph[nx][ny] = 1
+                q.append((nx, ny))
+                size += 1
+    result.append(size)
+
+result = []
+for i in range(m):
+    for j in range(n):
+        if graph[i][j] == 0:
+            bfs(i, j)
+            
+result.sort()
+
+print(len(result))
+print(*result)
