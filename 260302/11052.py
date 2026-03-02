2eb061daec22f8bb907b80dda723b8814d5e362e
kjy <kim0607mi@smail.kongju.ac.kr>
2026-03-02 14:21:33 +0900

    BOJ:11052

---
+++ 260302/11052.py
@@ -0,0 +1,10 @@
+# 백준 11052번
+
+n = int(input())
+dp = [0] + list(map(int, input().split()))
+
+for i in range(1, n+1):
+    for j in range(i):
+        dp[i] = max(dp[i], dp[j] + dp[i-j])
+
+print(dp[n])
